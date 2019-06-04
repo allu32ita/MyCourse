56ad64da7a40def24f27291105dcf342f310a2c0
allu32ita <aluigiandrea@gmail.com>
2019-06-04 13:18:07 +0200

    bootstrap

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace MyCourse
 {
@@ -20,11 +21,17 @@ namespace MyCourse
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
         {
             if (env.IsEnvironment("Development"))
             {
                 app.UseDeveloperExceptionPage();
+            
+                lifetime.ApplicationStarted.Register(() => {
+                    string filepath = Path.Combine(env.ContentRootPath, "bin/reload.txt");
+                    File.WriteAllText(filepath, DateTime.Now.ToString());
+                });
+                
             }
 
             app.UseStaticFiles();
